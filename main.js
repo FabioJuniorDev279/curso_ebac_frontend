15e9d1afb07240368ecd8cbd3ee040c86e160078
FabioJuniorDev279 <f.j321r@gmail.com>
2025-03-26 13:37:05 -0300

    Add main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+const formulario = document.getElementById('formulario')
+
+formulario.addEventListener('submit', function(e){
+    e.preventdefault();
+
+    const numeroUm = document.getElementById('camp-a')
+    const numeroDois = document.getElementById('camp-b')
+    const mensagemSucesso = document.getElementById('mensagem')
+
+});
